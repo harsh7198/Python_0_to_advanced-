14b0761c9656a0be00b674531a2739f6e84c25d8
harsh7198 <harshsuradkar2221@gmail.com>
2025-12-27 17:57:26 +0530

    String function

---
+++ string_function.py
@@ -0,0 +1,18 @@
+name = "harsh"
+
+print(name.title())
+print(name.upper())
+print(name.capitalize())
+print(name.lower())
+
+first_name = "Harsh"
+Last_name = "Suradkar"
+
+print(f"My name is {first_name} {Last_name}")
+
+
+name1 = "      Harsh     "
+
+print(name.strip()) # remove an extra space from string 
+
+print(f"My name is {name1.strip()}")
