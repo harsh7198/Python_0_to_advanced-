d007a2672796a63f45eaebfb2e72e1a05b968756
harsh7198 <harshsuradkar2221@gmail.com>
2025-12-28 15:12:52 +0530

    sum and multiplication

---
+++ sum_mul.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter First number : "))
+num2 = int(input("Enter second number : "))
+
+sum = num1 + num2
+mul = num1 * num2 
+
+print(f"Sum is : {sum}")
+print(f"Multiplaction is : {mul}")
