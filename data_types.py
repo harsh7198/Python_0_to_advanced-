daf93a966cc1912b42ef7285f5a556c50cd83fb7
harsh7198 <harshsuradkar2221@gmail.com>
2025-12-29 19:03:45 +0530

    Data types

---
+++ data_types.py
@@ -0,0 +1,25 @@
+name = "Harsh"
+print(type(name))
+
+number = 100
+print(type(number))
+
+float_1 = 2.0
+print(type(float_1))
+
+
+married = False
+print(type(married))
+
+cars = ["BMW", "MERCEDECS" , "BENTLY" , "TOYOTA"]
+print(type(cars))
+
+student  = {
+        "name " : "HARSH",
+        "marks" : 20,
+        "ROLL_no" : 16
+}
+print(type(student))
+
+tuple_1 = ("1", "2" , "3", "4", "5", "6")
+print(type(tuple_1))
