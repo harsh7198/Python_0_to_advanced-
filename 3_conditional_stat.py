d37269ecf0095e914e6c0e3e96ef3282d2687349
harsh7198 <harshsuradkar2221@gmail.com>
2025-12-29 19:27:15 +0530

    Conditional statment

---
+++ 3_conditional_stat.py
@@ -0,0 +1,21 @@
+# if statement 
+
+n = 10 
+
+if (n==10 or n<=10):
+    print("Value is 10 or below 10")
+
+else: 
+    print("Value is above 10")
+
+# ladder if elif 
+
+age = int(input("Enter Your age :"))
+if (age == 18):
+    print("Your age is : 18")
+
+elif(age <= 18):
+    print("Your age is below : 18 ")
+
+else:
+    print("Your age is above : 18")
