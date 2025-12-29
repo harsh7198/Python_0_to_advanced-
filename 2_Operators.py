6ca66265d0edd06279891e9dc5314da105c626d9
harsh7198 <harshsuradkar2221@gmail.com>
2025-12-29 19:21:49 +0530

    Operators

---
+++ 2_Operators.py
@@ -0,0 +1,43 @@
+# Arithmatic operator - + , - , % , / , ** , * 
+
+x = 20 
+y = 20 
+
+print(x+y)
+print(x-y)
+print(x*y)
+print((x**y))
+print(x/y)
+print(x%y)
+print("\n")
+# logical operator - and , or ,not
+
+A = True
+B = False
+print(A or B)
+print(A and B)
+print(not(A and B))
+print("\n")
+# comparison operators- <,> , <= , >= , == , != 
+
+print(x>y)
+print(x>=y)
+print(x<=y)
+print(x==y)
+print(x!=y)
+print(x<y)
+print("\n")
+# assignment operators - +=, -+ , *= , /= , **= , %= 
+
+b = 20 
+c = 10
+b += 20 
+print(b)
+c -= 10
+print(c) 
+
+print("\n")
+# membership operator
+days = ("Sunday" , "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday" )
+print('Sunday' in days)
+print('Sunday'  not in days)
