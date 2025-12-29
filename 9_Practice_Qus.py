efe015eaaad772c8fb59162bec772f226b8c4edd
harsh7198 <harshsuradkar2221@gmail.com>
2025-12-30 01:10:55 +0530

    practice Question for method

---
+++ 9_Practice_Qus.py
@@ -0,0 +1,16 @@
+# create a student class that takes name & marks of three subjects as argument in constructor
+# create a method for print avarage  
+
+class Student: 
+    def __init__(self, name , marks) :
+        self.name = name
+        self.marks = marks 
+    
+    def get_avg(self):
+        sum = 0
+        for val in self.marks:
+            sum += val
+        print("Hi,",self.name , "Your avg score is :", sum/3)
+
+s1 = Student("Tony", [99,98,97])
+s1.get_avg()
